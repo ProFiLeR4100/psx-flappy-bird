a4545cd6d7ccdb7837088bed9e883e7ec30f79f6
Mikhail Shcherbina <profiler4100v2@gmail.com>
2020-07-23 02:47:42 +0300

    refactored

--- MAKEFILE.mak
+++ MAKEFILE.mak
@@ -5,3 +5,4 @@ all:
 	
 	ccpsx -Xo0x80010000 -Wall -O2 src/*.cpp -I include -o main.cpe
 	cpe2x /ce main.cpe
+	mkpsxiso.exe -o game.iso -y cuesheet.xml
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Projects/PSYQ_SDK/demo/project/src/AudioService.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/AudioService.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/Bird.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/Bird.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/CDService.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/CDService.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/ColorHelper.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/ColorHelper.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/DisplayService.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/DisplayService.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/GamepadService.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/GamepadService.cpp.obj"
+  "C:/Projects/PSYQ_SDK/demo/project/src/main.cpp" "C:/Projects/PSYQ_SDK/demo/project/cmake-build-debug/CMakeFiles/untitled.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "C:/Projects/PSYQ_SDK/psyq/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
